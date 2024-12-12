0ac2336dfef733ff8975bfc15517a2c46c5985aa
Miguel Angel Martinez Ramirez <miguelmr560@hotmail.com>
2024-12-12 18:36:02 +0100

    initial commit

--- app/src/main/java/dam/pmdm/martinez_ramirez_miguel_angel_pmdm03/CapturadosFragment.java
+++ app/src/main/java/dam/pmdm/martinez_ramirez_miguel_angel_pmdm03/CapturadosFragment.java
@@ -28,7 +28,6 @@ public class CapturadosFragment extends Fragment {
     private RecyclerView recyclerView;
     private CapturadosRecyclerViewAdapter adapter;
     private ArrayList<CapturedPokemon> capturedPokemons; // Lista global para capturados
-    private SwitchPreferenceCompat eliminateModePref;
 
     @Nullable
     @Override
@@ -41,10 +40,9 @@ public class CapturadosFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        recyclerView = view.findViewById(R.id.capturados_recyclerview);  // Asegúrate de tener un RecyclerView en tu layout
+        recyclerView = view.findViewById(R.id.capturados_recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Obtener el Switch desde el archivo de preferencias
         // Accedemos a los SharedPreferences
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
         boolean eliminateModeEnabled = preferences.getBoolean("eliminate_mode", false);
@@ -62,61 +60,25 @@ public class CapturadosFragment extends Fragment {
                                 CapturedPokemon pokemon = document.toObject(CapturedPokemon.class);
                                 capturedPokemons.add(pokemon);
                             }
-                            // Aquí se pasa el Context además de la lista de Pokémon
                             adapter = new CapturadosRecyclerViewAdapter(getContext(), capturedPokemons);
                             recyclerView.setAdapter(adapter);
 
-                            // Configurar el listener de clic
-                            adapter.setOnItemClickListener((pokemon, holder) -> {
-                                deletePokemon(pokemon, holder, eliminateModeEnabled);
-                            });
+                            // Configurar el estado inicial del modo eliminar
+                            adapter.setEliminateModeEnabled(eliminateModeEnabled);
 
+                            // Observador de cambios en SharedPreferences
+                            preferences.registerOnSharedPreferenceChangeListener((sharedPreferences, key) -> {
+                                if ("eliminate_mode".equals(key)) {
+                                    boolean eliminateMode = sharedPreferences.getBoolean(key, false);
+                                    adapter.setEliminateModeEnabled(eliminateMode);
+                                }
+                            });
                         } else {
                             Log.e("Firebase", getString(R.string.query_error), task.getException());
                         }
                     });
         } else {
             Log.e("FirebaseAuth", getString(R.string.error_user_unknown));
-        }
-    }
-
-    private void deletePokemon(CapturedPokemon pokemon, CapturadosRecyclerViewAdapter.ViewHolder holder, boolean eliminateModeEnabled) {
-        if (eliminateModeEnabled) {
-            // Crear un AlertDialog para confirmar la eliminación
-            new AlertDialog.Builder(getContext())
-                    .setTitle(R.string.delete_confirm)
-                    .setMessage(getString(R.string.confirm_delete_text1) + pokemon.getName() + getString(R.string.confirm_delete_text2))
-                    .setPositiveButton(R.string.yes, (dialog, which) -> {
-                        // Si el usuario confirma, eliminar el Pokémon
-                        String userId = FirebaseAuth.getInstance().getUid();
-                        if (userId == null) {
-                            Log.e("Firestore", getString(R.string.error_user_unknown));
-                            return;
-                        }
-
-                        // Eliminar el documento del Pokémon usando su índice
-                        db.collection("captured_pokemon")
-                                .document(userId) // Documento del usuario
-                                .collection("pokemon") // Subcolección de Pokémon capturados
-                                .document(String.valueOf(pokemon.getIndex())) // Documento identificado por el índice del Pokémon
-                                .delete()
-                                .addOnSuccessListener(aVoid -> {
-                                    Log.d("Firestore", getString(R.string.pokemon_deleted));
-                                    // Eliminar de la lista local y actualizar el RecyclerView
-                                    int position = capturedPokemons.indexOf(pokemon);
-                                    capturedPokemons.remove(pokemon);
-                                    adapter.notifyItemRemoved(position);  // Notificar que el ítem fue eliminado
-                                })
-                                .addOnFailureListener(e -> Log.e("Firestore", getString(R.string.delete_error), e));
-                    })
-                    .setNegativeButton(R.string.no, (dialog, which) -> {
-                        // Si el usuario cancela, no hacer nada
-                        dialog.dismiss();
-                    })
-                    .show();
-        } else {
-            // Si el modo de eliminación no está activado, mostrar un Toast informando al usuario
-            Toast.makeText(getContext(), R.string.delete_blocked, Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/dam/pmdm/martinez_ramirez_miguel_angel_pmdm03/CapturadosRecyclerViewAdapter.java
+++ app/src/main/java/dam/pmdm/martinez_ramirez_miguel_angel_pmdm03/CapturadosRecyclerViewAdapter.java
@@ -1,16 +1,21 @@
 package dam.pmdm.martinez_ramirez_miguel_angel_pmdm03;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -19,7 +24,7 @@ public class CapturadosRecyclerViewAdapter extends RecyclerView.Adapter<Capturad
 
     private Context context;
     private ArrayList<CapturedPokemon> capturedPokemons;
-    private OnItemClickListener onItemClickListener;
+    private boolean eliminateModeEnabled = false; // Variable para controlar el modo de eliminación
 
     // Constructor
     public CapturadosRecyclerViewAdapter(Context context, ArrayList<CapturedPokemon> capturedPokemons) {
@@ -27,15 +32,10 @@ public class CapturadosRecyclerViewAdapter extends RecyclerView.Adapter<Capturad
         this.capturedPokemons = capturedPokemons;
     }
 
-    // Método para configurar el listener de clic
-    public void setOnItemClickListener(OnItemClickListener listener) {
-        this.onItemClickListener = listener;
-    }
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.pokemon_cardview, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.capturados_cardview, parent, false);
         return new ViewHolder(view);
     }
 
@@ -63,17 +63,46 @@ public class CapturadosRecyclerViewAdapter extends RecyclerView.Adapter<Capturad
             }
         }
 
-        // Detectar clic en un Pokémon y manejar el clic
-        holder.itemView.setOnClickListener(v -> {
-            if (onItemClickListener != null) {
-                onItemClickListener.onItemClick(pokemon, holder);
-            }
+        // Configurar visibilidad del botón delete_button en función del modo de eliminación
+        holder.deleteButton.setVisibility(eliminateModeEnabled ? View.VISIBLE : View.GONE);
+
+        // Configurar clic en el botón delete_button
+        holder.deleteButton.setOnClickListener(v -> {
+            // Confirmar y eliminar Pokémon
+            new AlertDialog.Builder(context)
+                    .setTitle(R.string.delete_confirm)
+                    .setMessage(context.getString(R.string.confirm_delete_text1) + pokemon.getName() + context.getString(R.string.confirm_delete_text2))
+                    .setPositiveButton(R.string.yes, (dialog, which) -> {
+                        String userId = FirebaseAuth.getInstance().getUid();
+                        if (userId != null) {
+                            FirebaseFirestore.getInstance()
+                                    .collection("captured_pokemon")
+                                    .document(userId)
+                                    .collection("pokemon")
+                                    .document(String.valueOf(pokemon.getIndex()))
+                                    .delete()
+                                    .addOnSuccessListener(aVoid -> {
+                                        capturedPokemons.remove(position);
+                                        notifyItemRemoved(position);
+                                        Log.d("Firestore", context.getString(R.string.pokemon_deleted));
+                                    })
+                                    .addOnFailureListener(e -> Log.e("Firestore", context.getString(R.string.delete_error), e));
+                        }
+                    })
+                    .setNegativeButton(R.string.no, null)
+                    .show();
         });
     }
 
     @Override
     public int getItemCount() {
         return capturedPokemons.size();
+    }
+
+    // Método para actualizar el modo de eliminación
+    public void setEliminateModeEnabled(boolean enabled) {
+        this.eliminateModeEnabled = enabled;
+        notifyDataSetChanged(); // Notificar cambios en los datos para redibujar las vistas
     }
 
     // Método para obtener el ícono correspondiente a cada tipo
@@ -83,24 +112,42 @@ public class CapturadosRecyclerViewAdapter extends RecyclerView.Adapter<Capturad
         }
 
         switch (type.toLowerCase()) {
-            case "fire": return R.drawable.ic_fire;
-            case "water": return R.drawable.ic_water;
-            case "grass": return R.drawable.ic_grass;
-            case "electric": return R.drawable.ic_electric;
-            case "ice": return R.drawable.ic_ice;
-            case "fighting": return R.drawable.ic_fighting;
-            case "poison": return R.drawable.ic_poison;
-            case "ground": return R.drawable.ic_ground;
-            case "flying": return R.drawable.ic_flying;
-            case "psychic": return R.drawable.ic_psychic;
-            case "bug": return R.drawable.ic_bug;
-            case "rock": return R.drawable.ic_rock;
-            case "ghost": return R.drawable.ic_ghost;
-            case "dark": return R.drawable.ic_dark;
-            case "dragon": return R.drawable.ic_dragon;
-            case "steel": return R.drawable.ic_steel;
-            case "fairy": return R.drawable.ic_fairy;
-            default: return R.drawable.ic_default;
+            case "fire":
+                return R.drawable.ic_fire;
+            case "water":
+                return R.drawable.ic_water;
+            case "grass":
+                return R.drawable.ic_grass;
+            case "electric":
+                return R.drawable.ic_electric;
+            case "ice":
+                return R.drawable.ic_ice;
+            case "fighting":
+                return R.drawable.ic_fighting;
+            case "poison":
+                return R.drawable.ic_poison;
+            case "ground":
+                return R.drawable.ic_ground;
+            case "flying":
+                return R.drawable.ic_flying;
+            case "psychic":
+                return R.drawable.ic_psychic;
+            case "bug":
+                return R.drawable.ic_bug;
+            case "rock":
+                return R.drawable.ic_rock;
+            case "ghost":
+                return R.drawable.ic_ghost;
+            case "dark":
+                return R.drawable.ic_dark;
+            case "dragon":
+                return R.drawable.ic_dragon;
+            case "steel":
+                return R.drawable.ic_steel;
+            case "fairy":
+                return R.drawable.ic_fairy;
+            default:
+                return R.drawable.ic_default;
         }
     }
 
@@ -111,6 +158,7 @@ public class CapturadosRecyclerViewAdapter extends RecyclerView.Adapter<Capturad
         private final TextView heightTextView;
         private final TextView weightTextView;
         private final LinearLayout typesContainer;
+        private final Button deleteButton; // Botón de eliminar
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -120,11 +168,7 @@ public class CapturadosRecyclerViewAdapter extends RecyclerView.Adapter<Capturad
             heightTextView = itemView.findViewById(R.id.height);
             weightTextView = itemView.findViewById(R.id.weight);
             typesContainer = itemView.findViewById(R.id.types_layout);
+            deleteButton = itemView.findViewById(R.id.delete_button); // Vincula el botón al ViewHolder
         }
-    }
-
-    // Interfaz para el callback del clic en ítem
-    public interface OnItemClickListener {
-        void onItemClick(CapturedPokemon pokemon, ViewHolder holder);
     }
 }
